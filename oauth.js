096efe2b21e91efca08e1a2b5e41e5b722a2513d
erickstm87 <dev@terickson.io>
2019-10-15 14:37:17 -0600

    first commit

---
+++ oauth.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    document.querySelector('button').addEventListener('click', function() {
+      
+      chrome.identity.getAuthToken({interactive: true}, function(token) {
+        console.log(token);
+      });
+
+      chrome.identity.getProfileUserInfo((info) => {
+        console.log(info);
+      })
+    });
+  };
